6d313a762d3c8daa20877a2b413f568588765ce8
Nikola Horvat <the.final.frontend@gmail.com>
2019-09-09 18:35:55 +0200

    feat: add rules for cells

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -4,9 +4,9 @@ import { Cell } from "./Cell";
 function Grid() {
   const [matrix, setMatrix] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
-    [0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+    [0, 0, 1, 1, 1, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -15,6 +15,31 @@ function Grid() {
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
   ]);
 
+  const countLiveCells = (x: number, y: number): number => {
+    const tL = matrix[x - 1] ? (matrix[y - 1] ? matrix[x - 1][y - 1] : 0) : 0;
+    const tM = matrix[x - 1] ? matrix[x - 1][y] : 0;
+    const tR = matrix[x - 1]
+      ? matrix[x - 1][y + 1]
+        ? matrix[x - 1][y + 1]
+        : 0
+      : 0;
+    const mL = matrix[x][y - 1] ? matrix[x][y - 1] : 0;
+    const mR = matrix[x][y + 1] ? matrix[x][y + 1] : 0;
+    const bL = matrix[x + 1]
+      ? matrix[x + 1][y - 1]
+        ? matrix[x + 1][y - 1]
+        : 0
+      : 0;
+    const bM = matrix[x + 1] ? matrix[x + 1][y] : 0;
+    const bR = matrix[x + 1]
+      ? matrix[x + 1][y + 1]
+        ? matrix[x + 1][y + 1]
+        : 0
+      : 0;
+
+    return tL + tM + tR + mL + mR + bL + bM + bR;
+  };
+
   const cells = matrix.map((row: number[], rowIndex: number) =>
     row.map((col: number, colIndex: number) => (
       <Cell isAlive={!!col} key={`${rowIndex}${colIndex}`} />
@@ -22,8 +47,18 @@ function Grid() {
   );
 
   const testCell = (x: number, y: number, currentVal: number) => {
-    console.log("x:", x, "y:", y, "currentVal:", currentVal);
-    // reset to zero
+    let liveNeibhors = countLiveCells(x, y);
+    if (currentVal === 1) {
+      if (liveNeibhors < 2 || liveNeibhors > 3) {
+        return 0;
+      }
+      return 1;
+    } else if (currentVal === 0) {
+      if (liveNeibhors === 3) {
+        return 1;
+      }
+      return 0;
+    }
     return 0;
   };
 
@@ -33,7 +68,6 @@ function Grid() {
         testCell(row_index, cell_index, currentVal)
       )
     );
-
     setMatrix(matrixNext);
   };
 
